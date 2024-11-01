665bb2d1f92616f0e7da8174a45c8a88cf6aca39
Kiaan <kiaan24@tutanota.com>
2024-11-01 12:09:36 +0000

    Update vending machine.py

--- vending machine.py
+++ vending machine.py
@@ -1,12 +1,13 @@
-#Python 3.3 Vending Machine
-#By ZenOokami
+#Toki Pona Vending Machine
+#Based on ZenOokami's code
 #www.EssenceOfZen.org
 #Version 1.0
 
+start = "\033[1m"
+end = "\033[0;0m"
 money = 10.00 #This will be the money used to purchase the snacks
-vending_machine = {"Chips":"0A", "Soda":"0B", "Chocolate":"0C", "Fruit Snacks":"0D"} #We created this hash (python calls them dictionaries) to list the items and their selection
-prices = {"Chips":"$0.75", "Soda":"$0.50", "Chocolate":"$1.25", "Fruit Snacks":"$.25"} #We created this hash to have the same keys as the first hash, but this time having prices be their values
-
+vending_machine = {"pan suwi":"0A", "telo suwi":"0B", "telo pi lape ala":"0C", "kili lili":"0D", "moku soweli":"0E", "namako 1 GBP lon poki sina":"1A", "namako 2 GBP lon poki sina":"1B", "namako 5 GBP lon poki sina":"1C", "namako 10 GBP lon poki sina":"1D", "namako 20 GBP lon poki sina":"1E", "namako mani wile lon poki sina":"1F"} #We created this hash (python calls them dictionaries) to list the items and their selection
+prices = {"pan suwi":"0.75 GBP", "telo suwi":"0.50 GBP", "telo pi lape ala":"1.25 GBP", "kili lili":"0.25 GBP", "moku soweli":"0.75 GBP", "namako "+start+"1 GBP"+end+" lon poki sina":"", "namako "+start+"2 GBP"+end+" lon poki sina":"", "namako "+start+"5 GBP"+end+" lon poki sina":"", "namako "+start+"10 GBP"+end+" lon poki sina":"", "namako "+start+"20 GBP"+end+" lon poki sina":"", "namako mani wile lon poki sina":""} #We created this hash to have the same keys as the first hash, but this time having prices be their values
 
 #==Debug Functions==================================
 def set_money(amount): #This function is used to set the amount of money
@@ -16,21 +17,13 @@ def set_money(amount): #This function is used to set the amount of money
 
 
 #===================================================
-#==Normal Functions=================================
-def get_stock(): #Old Function -- do not use. It functioned when I was keep track of number of stocks
-	stock = vending_machine.values()
-	i = 0
-	for amount in stock:
-		i += amount
-	print(i)
-	
 def purchase(needed_money): #Made this for personal ease. It simply keeps track of money after checking to see if you can buy said item.
 	global money
 	if money >= needed_money:
 		money -= needed_money
-		print("Item Vended.")
+		print("ijo pana")
 	else:
-		print("Not enough money.")
+		print("ni li mani wile ala")
 
 def transaction(user_input): #Function is made to take care of choosing said item, it then calls the "purchase" funciton
 	global money
@@ -42,8 +35,24 @@ def transaction(user_input): #Function is made to take care of choosing said ite
 		purchase(1.25)
 	elif user_input == "0D":
 		purchase(0.25)
+	elif user_input == "0E":
+		purchase(0.75)
+	elif user_input == "1A":
+		purchase(-1)
+	elif user_input == "1B":
+		purchase(-2)
+	elif user_input == "1C":
+		purchase(-5)
+	elif user_input == "1D":
+		purchase(-10)
+	elif user_input == "1E":
+		purchase(-20)
+	elif user_input == "1F":
+		maniWile = float(input("mani la mute seme li sina wile lon poki sina?")) #Adds a desired value to your account
+		maniWile = maniWile*-1
+		purchase(maniWile)
 	else:
-		print("Invalid Input")
+		print("sina sitelen li pakala")
 	
 			
 #==============================================================	
@@ -51,51 +60,45 @@ def transaction(user_input): #Function is made to take care of choosing said ite
 
 def main(): #Main program 
 	item_list = []
-	switch = 1 #Incase you want to add a "different person" system
+	switch = 1 #In case you want to add a "different person" system
 	while switch == 1:
-		print("Welcome to the Vending Machine program.") #Welcome Messages
-		print("Here are your selections!")
+		print("toki a! ni li ilo wile.") #Welcome Messages
+		print("ni li sona sina!")
 		for item, selection in vending_machine.items(): #This for loop will append items to the list item_list
 			item_list.append((item, selection))
 		
 		print(item_list[::-1]) #Print backwards so that it goes from top to the bottom.
 		
 		print()
-		print("Here are the prices")
+		print("ni li mani esun")
 		for item, price in prices.items(): #Prints the items and their prices on separate lines.
 			print(item, price)
 		print()	
 		
 		user_switch = 1 #User Proof loop
 		while user_switch == 1:
-			print("You currently have $" + str(money))
-			user_input = input("Please input your selction: ").upper()
+			print("sina jo e " + str(money) + " GBP")
+			user_input = input("mi wile poki sona sina: ").upper()
 			transaction(user_input)
 			print()
 			choice = 1
 			while choice == 1: #User proof loop
-				user_input = input("Are you finished with the vending?(y/n): ").lower()
-				if user_input == "y":
+				user_input = input("sina pini kepeken ni anu seme?(k/a): ").lower()
+				if user_input == "k" or user_input == "kepeken":
 					user_switch = 0
 					choice = 0
 					switch = 0
-				elif user_input == "n":
+				elif user_input == "a" or user_input == "ala":
 					user_switch = 1
 					choice = 0
 				else:
-					print("Invalid command")
+					print("sina sitelen li pakala")
 					choice = 1
 			
-		print("Thank you for using this program to either learn or for practice.")
-		print("If you have any quesitons, please visit the forums over at www.EssenceOfZen.org/")
-		print("-ZenOokami")
+		print("sina pona tawa kepeken ni ilo.")
+		print("mi mama ni kepeken sitelen lawa pi jan ZenOokami")
 					
 				
 			
-			
-			
-			
-			
 		
 main()
-		
