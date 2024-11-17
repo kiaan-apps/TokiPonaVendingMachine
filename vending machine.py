afe4eba71f08d9d9d2b833133155b05539cac290
Kiaan <kiaan24@tutanota.com>
2024-11-17 20:32:51 +0000

    Version 1.2
    We have added 5 more items, inserting money and the option to buy an egg that costs 150GBP and includes insurance in case it breaks

--- vending machine.py
+++ vending machine.py
@@ -1,13 +1,13 @@
 #Toki Pona Vending Machine
 #Based on ZenOokami's code
 #www.EssenceOfZen.org
-#Version 1.1
+#Version 1.2
 
 start = "\033[1m" #The start and end variables are to make bold text in Python
 end = "\033[0;0m"
-money = 10.00 #This will be the money used to purchase the snacks
-vending_machine = {"pan suwi":"0A", "telo suwi":"0B", "telo pi lape ala":"0C", "kili lili":"0D", "moku soweli":"0E", "namako e 1 GBP lon poki sina":"1A", "namako e 2 GBP lon poki sina":"1B", "namako e 5 GBP lon poki sina":"1C", "namako e 10 GBP lon poki sina":"1D", "namako e 20 GBP lon poki sina":"1E", "namako e mani wile lon poki sina":"1F"} #We created this hash (python calls them dictionaries) to list the items and their selection
-prices = {"pan suwi":"0.75 GBP", "telo suwi":"0.50 GBP", "telo pi lape ala":"1.25 GBP", "kili lili":"0.25 GBP", "moku soweli":"0.75 GBP", "namako e "+start+"1 GBP"+end+" lon poki sina":"", "namako e "+start+"2 GBP"+end+" lon poki sina":"", "namako e "+start+"5 GBP"+end+" lon poki sina":"", "namako e "+start+"10 GBP"+end+" lon poki sina":"", "namako e "+start+"20 GBP"+end+" lon poki sina":"", "namako e mani wile lon poki sina":""} #We created this hash to have the same keys as the first hash, but this time having prices be their values
+money = float(input("mani mute seme li sina wile lon poki sina?")) #This will be the money used to purchase the snacks
+vending_machine = {"pan suwi":"0A", "telo suwi":"0B", "telo pi lape ala":"0C", "kili lili":"0D", "moku soweli":"0E", "kili ma":"0F", "namako e 1 GBP lon poki sina":"1A", "namako e 2 GBP lon poki sina":"1B", "namako e 5 GBP lon poki sina":"1C", "namako e 10 GBP lon poki sina":"1D", "namako e 20 GBP lon poki sina":"1E", "namako e mani wile lon poki sina":"1F", "kili kiki":"2A", "telo nasa":"2B", "telo walo kiwen":"2C", "kili loje":"2D", "kili sike lili":"2E", "moku walo sike - mi namako e selo awen":"2F"} #We created this hash (python calls them dictionaries) to list the items and their selection
+prices = {"pan suwi":"0.75 GBP", "telo suwi":"0.50 GBP", "telo pi lape ala":"1.25 GBP", "kili lili":"0.25 GBP", "moku soweli":"0.75 GBP", "kili ma":"0.02 GBP", "namako e "+start+"1 GBP"+end+" lon poki sina":"", "namako e "+start+"2 GBP"+end+" lon poki sina":"", "namako e "+start+"5 GBP"+end+" lon poki sina":"", "namako e "+start+"10 GBP"+end+" lon poki sina":"", "namako e "+start+"20 GBP"+end+" lon poki sina":"", "namako e mani wile lon poki sina":"", "kili kiki":"5 GBP", "telo nasa pi ma":"6 GBP", "telo walo kiwen":"2.50 GBP", "ale pi kili loje":"3 GBP", "kili sike lili":"1.75 GBP", "moku walo sike - mi namako e selo awen":"150 GBP"} #We created this hash to have the same keys as the first hash, but this time having prices be their values
 
 #==Debug Functions==================================
 def set_money(amount): #This function is used to set the amount of money
@@ -22,7 +22,7 @@ def purchase(needed_money): #Made this for personal ease. It simply keeps track 
 		money -= needed_money
 		print("ijo pana")
 	else:
-		print("ni li mani wile ala")
+		print("sina jo ala e mani wile")
 
 def transaction(user_input): #Function is made to take care of choosing said item, it then calls the "purchase" funciton
 	global money
@@ -36,6 +36,8 @@ def transaction(user_input): #Function is made to take care of choosing said ite
 		purchase(0.25)
 	elif user_input == "0E":
 		purchase(0.75)
+	elif user_input == "0F":
+		purchase(0.02)
 	elif user_input == "1A":
 		purchase(-1)
 	elif user_input == "1B":
@@ -50,6 +52,19 @@ def transaction(user_input): #Function is made to take care of choosing said ite
 		maniWile = float(input("mani mute seme li sina wile lon poki sina?")) #Adds a desired value to your account
 		maniWile = maniWile*-1
 		purchase(maniWile)
+	elif user_input == "2A":
+		purchase(5)
+	elif user_input == "2B":
+		purchase(6)
+	elif user_input == "2C":
+		purchase(2.5)
+	elif user_input == "2D":
+		purchase(3)
+	elif user_input == "2E":
+		purchase(1.75)
+	elif user_input == "2F":
+		purchase(150)
+		print("tenpo ni la, sina esun e selo awen pi epiku mute en moku walo sike. ona li pakala la, mi ante e ona kan ala e mani.")
 	else:
 		print("sina sitelen li pakala")
 	
